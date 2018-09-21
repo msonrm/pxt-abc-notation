4a307561302eb0b141fd7149341763f3d407ce0d
Narumi Masao <42761436+msonrm@users.noreply.github.com>
2018-09-21 23:36:22 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\uf075" color=#555555 block="コメント"
+namespace abcNotation {
+    //% blockId=show_strings block="ABC notation %v"
+    export function noAction(text: string): void {
+    }
+}
