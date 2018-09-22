6e9231f073cdd4114cc975f8e5b7225b85eab6fc
Narumi Masao <42761436+msonrm@users.noreply.github.com>
2018-09-23 00:08:47 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% weight=70 icon="\uf001" color=#ffa500 block="ABC notation"
 namespace abcNotation {
-    //% blockId=show_strings block="ABC notation2 %v"
+    //% blockId=show_strings block="ABC notation %v"
     export function noAction(text: string): void {
     }
 }
