b68ec1f76d97b30cf019f15e48db2a0d4a3ea9e1
Narumi Masao <42761436+msonrm@users.noreply.github.com>
2018-09-25 22:24:22 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
 //% weight=70 icon="\uf001" color=#ffa500 block="ABC notation"
 namespace abcNotation {
-    
+
     //% blockId=play_notes block="play notes %v"
     export function playNotes(text: string): void {
         pins.analogPitch(262, 100)
     }
-    
-    //% blockId=set_key block="set key(K:) to %key"
-    export function key() {
-        
+
+
     }
 
 }
