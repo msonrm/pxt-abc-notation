71c544b1246d9fe2ad52de67037724663b3beed4
Narumi Masao <42761436+msonrm@users.noreply.github.com>
2018-10-17 13:08:30 +0900

    fix init()

--- main.ts
+++ main.ts
@@ -103,6 +103,7 @@ namespace abcNotation {
         if (beatNote <= 0) beatNote = 1 / 4;
         if (bpm <= 0) bpm = 120;
         if (unitNoteMs <= 0) unitNoteMs = (60000 / bpm) * (unitNote / beatNote);
+        if (key = []) key = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         if (freqTable.length == 0) freqTable = [65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 0]
     }
 
@@ -248,7 +249,7 @@ namespace abcNotation {
             currNote = currMeasure[currNotePos];
             currAccidental = null;
             isrest = false;
-            duration = unitNoteMs / unitNote;
+            duration = unitNoteMs;
             for (let pos = 0; pos < currNote.length; pos++) {
                 let noteChar = currNote.charAt(pos);
                 beatPos = 0;
