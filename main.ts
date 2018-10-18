b736d839340be2d5fb4e81524bf5f5773b37fa09
Narumi Masao <42761436+msonrm@users.noreply.github.com>
2018-10-18 18:21:46 +0900

    test key

--- main.ts
+++ main.ts
@@ -103,7 +103,7 @@ namespace abcNotation {
         if (beatNote <= 0) beatNote = 1 / 4;
         if (bpm <= 0) bpm = 120;
         if (unitNoteMs <= 0) unitNoteMs = (60000 / bpm) * (unitNote / beatNote);
-        if (key = []) key = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        if (key.length == 0) key = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         if (freqTable.length == 0) freqTable = [0, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951]
     }
 
@@ -162,7 +162,7 @@ namespace abcNotation {
      * Sets the minor key.
      */
     //% blockId=set_minor_key block="set minor key (K:) %setKey"
-    export function setMinorKey(setKey?: KeyNameMinor): void {
+    export function setMinorKey(setKey: KeyNameMinor): void {
         switch (setKey) {
             case 1: key = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
             case 2: key = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0];
@@ -244,10 +244,11 @@ namespace abcNotation {
             })
         }
 
-//test
-key.forEach(function (value: number) {
-    basic.showNumber(value);
-})
+        //test
+        key.forEach(function (value: number) {
+            basic.showNumber(value);
+            basic.showIcon(IconNames.Heart);
+        })
 
         for (currNotePos = 0; currNotePos < currMeasure.length; currNotePos++) {
             currNote = currMeasure[currNotePos];
@@ -300,3 +301,4 @@ key.forEach(function (value: number) {
         }
     }
 }
+
